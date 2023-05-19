cfc40cf552e84efa7124f123e8cc13dc229a2a06
islam-ahsanul <ahsanul.islam@g.bracu.ac.bd>
2023-05-19 11:51:55 +0600

    Global state initializing

---
+++ src/context/AppReducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// action has type, payload
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default AppReducer;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import React, { Children, createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// initialState
+const initialState = {
+  transactons: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: -20 },
+    { id: 3, text: 'Book', amount: -20 },
+    { id: 4, text: 'Camera', amount: -20 },
+  ],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactons: state.transactons,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
